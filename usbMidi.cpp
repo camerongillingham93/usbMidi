d78ab0d6b1774c5c9abde65ae0b4d181e2006124
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-01-08 21:46:46 -0800

    improved code, but release is still not working. will be easier with de-bugger.

--- usbMidi.cpp
+++ usbMidi.cpp
@@ -6,10 +6,10 @@
  *  To keep the example short, only note on messages are handled, and there
  *  is only a single oscillator voice that tracks the most recent note message.
  */
+
 #include "daisy_seed.h"
 #include "daisysp.h"
-#include <unordered_map>
-
+#include <vector>
 
 using namespace daisy;
 using namespace daisysp;
@@ -18,13 +18,23 @@ DaisySeed      hw;
 MidiUsbHandler midi;
 Oscillator     osc;
 
-// Add a data structure to keep track of note states
-std::unordered_map<int, bool> noteStates;
+// Vector to Store Midi note events
+std::vector<daisy::NoteOnEvent> keys;
+
+// Function Prototypes
+void handleMidiMessage(daisy::MidiEvent m);
+void keyPressed(daisy::NoteOnEvent on);
+void keyReleased(daisy::NoteOnEvent off);
+void setOsc();
 
 void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
                    size_t                    size)
 {
+    // Set the oscillator frequency and amplitude based on MIDI note events
+    setOsc();
+    
+    // Process the audio with the oscillator
     for(size_t i = 0; i < size; i++)
         out[0][i] = out[1][i] = osc.Process();
 }
@@ -36,62 +46,88 @@ int main(void)
     hw.Init();
 
     /** Initialize USB Midi 
-		 *  by default this is set to use the built in (USB FS) peripheral.
-		 * 
-		 *  by setting midi_cfg.transport_config.periph = MidiUsbTransport::Config::EXTERNAL
-		 *  the USB HS pins can be used (as FS) for MIDI 
-		 */
+     *  By default, this is set to use the built-in (USB FS) peripheral.
+     *  By setting midi_cfg.transport_config.periph = MidiUsbTransport::Config::EXTERNAL,
+     *  the USB HS pins can be used (as FS) for MIDI 
+     */
     MidiUsbHandler::Config midi_cfg;
     midi_cfg.transport_config.periph = MidiUsbTransport::Config::INTERNAL;
     midi.Init(midi_cfg);
+    midi.StartReceive();
 
     /** Initialize our test tone */
     osc.Init(hw.AudioSampleRate());
 
-    /** And start the audio callback */
+    /** Start the audio callback */
     hw.StartAudio(AudioCallback);
 
-    // Initialize noteStates map
-    noteStates = std::unordered_map<int, bool>();
+    
 
     while(1)
     {
         /** Listen to MIDI for new changes */
         midi.Listen();
 
-        /** When there are messages waiting in the queue... */
         while(midi.HasEvents())
         {
-  /** Pull the oldest one from the list... */
-            auto msg = midi.PopEvent();
-            switch(msg.type)
-            {
-                case NoteOn:
-                {
-                    /** and change the frequency of the oscillator */
-                    auto note_msg = msg.AsNoteOn();
-                    if(note_msg.velocity != 0)
-                    {
-                        // Map the velocity to a suitable amplitude range (e.g., 0.0 to 1.0)
-                        float amplitude
-                            = static_cast<float>(note_msg.velocity) / 127.0f;
-
-                        osc.SetFreq(mtof(note_msg.note));
-                        osc.SetAmp(amplitude);
-                    }
-                }
-                break;
-                case NoteOff:
-                {
-                    /** Handle Note Off message */
-                    auto note_msg = msg.AsNoteOff();
-                    osc.SetFreq(0.0f);
-                }
-                default: break;
-            }
+            // Handle MIDI events
+            handleMidiMessage(midi.PopEvent());
         }
     }
 }
 
+// Function Definitions
 
+void handleMidiMessage(daisy::MidiEvent m)
+{
+    switch(m.type)
+    {
+        // Handle Note On events
+        case daisy::NoteOn:
+        {
+            keyPressed(m.AsNoteOn());
+        }
+        break;
+        // Handle Note Off events
+        case NoteOff:
+        {
+            keyReleased(m.AsNoteOn());
+        }
+        break;
+        default: break;
+    }
+}
+
+void keyPressed(daisy::NoteOnEvent on)
+{
+    // Store the Note On event in the keys vector
+    keys.push_back(on);
+}
+
+void keyReleased(daisy::NoteOnEvent off)
+{
+    keys.erase(std::remove_if(keys.begin(),
+                              keys.end(),
+                              [off](const daisy::NoteOnEvent& k)
+                              { return k.note == off.note; }),
+               keys.end());
+}
+
+
+void setOsc()
+{
+    // Iterate through the stored Note On events and set the oscillator parameters
+    for(const auto& key : keys)
+    {
+        // Accessing the note number of the key
+        int noteNumber = key.note;
+
+        // Accessing the velocity/amplitude of the key
+        float amplitude = static_cast<float>(key.velocity) / 127.0f;
+
+        // Set the oscillator frequency and amplitude
+        osc.SetFreq(mtof(noteNumber));
+        osc.SetAmp(amplitude);
+    }
+}
 
